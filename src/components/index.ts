92d336b82db78653a0d12c21ba7a0ae26179dd85
Justinas Reigis <justinas.reigis@gmail.com>
2020-04-26 16:00:58 +0300

    feat: add image picker for new ad form

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as AdItem } from './AdItem/AdItem';
 export { default as Separator } from './Separator/Separator';
+export { default as ImagePicker } from './ImagePicker/ImagePicker';
--- src/screens/PostNewScreen/PostNewScreen.tsx
+++ src/screens/PostNewScreen/PostNewScreen.tsx
@@ -1,66 +1,78 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
 import { Formik } from 'formik';
-import { View, Item, Button, Container, Content, Textarea, Input, Text } from 'native-base';
-import { StyleSheet } from 'react-native';
-import { Image } from 'react-native';
+import { Button, Container, Content, Input, Item, Text, Textarea, View } from 'native-base';
+import React from 'react';
+import { Image, StyleSheet } from 'react-native';
+import { ImagePicker } from '../../components';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export const PostNewScreen: React.FC = () => (
   <Container>
     <Content padder contentContainerStyle={styles.flex}>
-      <Formik
-        initialValues={{ title: '', description: '' }}
-        onSubmit={values => console.log(values)}
-      >
-        {({ handleChange, handleBlur, handleSubmit, values }) => (
-          <View style={styles.form}>
-            <View style={styles.grow}>
-              <Item regular>
-                <Input
-                  onChangeText={handleChange('title')}
-                  onBlur={handleBlur('title')}
-                  value={values.title}
-                  placeholder="title"
+      <ScrollView>
+        <Formik
+          initialValues={{ title: '', description: '', price: 0 }}
+          onSubmit={values => console.log(values)}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values }) => (
+            <View style={styles.form}>
+              <View style={styles.grow}>
+                <Item regular>
+                  <Input
+                    onChangeText={handleChange('title')}
+                    onBlur={handleBlur('title')}
+                    value={values.title}
+                    placeholder="title"
+                  />
+                </Item>
+                <Textarea
+                  rowSpan={5}
+                  underline={false}
+                  bordered
+                  placeholder="Description"
+                  onChangeText={handleChange('description')}
                 />
-              </Item>
-              <Textarea
-                rowSpan={5}
-                underline={false}
-                bordered
-                placeholder="Description"
-                onChangeText={handleChange('description')}
-              />
-              <View style={{ flexDirection: 'row', height: 100, justifyContent: 'space-evenly' }}>
-                <Image
-                  source={{ uri: 'https://picsum.photos/100' }}
-                  resizeMode="contain"
-                  style={{ flex: 1, margin: 10 }}
-                />
-                <Image
-                  source={{ uri: 'https://picsum.photos/300' }}
-                  resizeMode="contain"
-                  style={{ flex: 1, margin: 10 }}
-                />
-                <Image
-                  source={{ uri: 'https://picsum.photos/200' }}
-                  resizeMode="contain"
-                  style={{ flex: 1, margin: 10 }}
-                />
-                <Image
-                  source={{ uri: 'https://picsum.photos/400' }}
-                  resizeMode="contain"
-                  style={{ flex: 1, margin: 10 }}
-                />
+                <View style={{ flexDirection: 'row', height: 100, justifyContent: 'space-evenly' }}>
+                  <Image
+                    source={{ uri: 'https://picsum.photos/100' }}
+                    resizeMode="contain"
+                    style={{ flex: 1, margin: 10 }}
+                  />
+                  <Image
+                    source={{ uri: 'https://picsum.photos/300' }}
+                    resizeMode="contain"
+                    style={{ flex: 1, margin: 10 }}
+                  />
+                  <Image
+                    source={{ uri: 'https://picsum.photos/200' }}
+                    resizeMode="contain"
+                    style={{ flex: 1, margin: 10 }}
+                  />
+                  <Image
+                    source={{ uri: 'https://picsum.photos/400' }}
+                    resizeMode="contain"
+                    style={{ flex: 1, margin: 10 }}
+                  />
+                </View>
+                <Item regular>
+                  <Input
+                    onChangeText={handleChange('price')}
+                    onBlur={handleBlur('price')}
+                    value={values.title}
+                    placeholder="price"
+                  />
+                </Item>
+                <ImagePicker />
+              </View>
+              <View>
+                <Button onPress={handleSubmit} style={{ justifyContent: 'center' }}>
+                  <Text>Submit</Text>
+                </Button>
               </View>
             </View>
-            <View>
-              <Button onPress={handleSubmit} style={{ justifyContent: 'center' }}>
-                <Text>Submit</Text>
-              </Button>
-            </View>
-          </View>
-        )}
-      </Formik>
+          )}
+        </Formik>
+      </ScrollView>
     </Content>
   </Container>
 );
