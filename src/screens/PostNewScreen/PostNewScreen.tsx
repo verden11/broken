e5f71eb006f11874c866e51e79a37cd85197c643
Justinas Reigis <justinas.reigis@gmail.com>
2020-05-01 19:41:20 +0300

    style: post new ad screen

--- src/screens/PostNewScreen/PostNewScreen.tsx
+++ src/screens/PostNewScreen/PostNewScreen.tsx
@@ -15,77 +15,94 @@ import {
   View,
 } from 'native-base';
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, useWindowDimensions, ImageBackground } from 'react-native';
 import { ImgPicker } from '../../components';
 
-export const PostNewScreen: React.FC = () => (
-  <Container>
-    <Header>
-      <Body>
-        <Title>Post new Screen</Title>
-        <Subtitle>crate new listing</Subtitle>
-      </Body>
-    </Header>
-    <Content padder contentContainerStyle={styles.flex}>
-      <View style={styles.container}>
-        <Text style={styles.textCenter}>Post a new Item</Text>
-        <Formik
-          initialValues={{ title: '', description: '', price: 0 }}
-          onSubmit={values => console.log(values)}
+export const PostNewScreen: React.FC = () => {
+  const windowWidth = Math.round(useWindowDimensions().width);
+  const windowHeight = Math.round(useWindowDimensions().height);
+
+  return (
+    <Container>
+      <Header>
+        <Body>
+          <Title>Post new Screen</Title>
+          <Subtitle>crate new listing</Subtitle>
+        </Body>
+      </Header>
+      <Content padder scrollEnabled contentContainerStyle={{ flexGrow: 1 }}>
+        <ImageBackground
+          source={{ uri: `https://picsum.photos/${windowWidth}/${windowHeight}` }}
+          style={styles.flex}
+          resizeMode="cover"
+          imageStyle={styles.opacity}
         >
-          {({ handleChange, handleBlur, handleSubmit, values }) => (
-            <Form style={styles.grow}>
-              <View style={styles.grow}>
-                <Item regular>
-                  <Input
-                    onChangeText={handleChange('title')}
-                    onBlur={handleBlur('title')}
-                    value={values.title}
-                    placeholder="title"
-                  />
-                </Item>
-                <Textarea
-                  rowSpan={5}
-                  underline={false}
-                  bordered
-                  placeholder="Description"
-                  onChangeText={handleChange('description')}
-                />
-                <View style={styles.imageRow}>
-                  <ImgPicker />
-                  <ImgPicker />
-                  <ImgPicker />
-                  <ImgPicker />
-                </View>
-                <Item regular>
-                  <Input
-                    onChangeText={handleChange('price')}
-                    onBlur={handleBlur('price')}
-                    value={values.title}
-                    placeholder="price"
-                  />
-                </Item>
-              </View>
-              <View>
-                <Button onPress={handleSubmit} style={styles.justifyCenter}>
-                  <Text>Submit</Text>
-                </Button>
-              </View>
-            </Form>
-          )}
-        </Formik>
-      </View>
-    </Content>
-  </Container>
-);
+          <View style={styles.container}>
+            <Text style={styles.formTitle}>Post a new Item</Text>
+            <Formik
+              initialValues={{ title: '', description: '', price: '' }}
+              onSubmit={values => console.log(values)}
+            >
+              {({ handleChange, handleBlur, handleSubmit, values }) => (
+                <Form style={styles.grow}>
+                  <View style={styles.grow}>
+                    <Item regular>
+                      <Input
+                        onChangeText={handleChange('title')}
+                        onBlur={handleBlur('title')}
+                        value={values.title}
+                        placeholder="title"
+                      />
+                    </Item>
+                    <Textarea
+                      rowSpan={5}
+                      underline={false}
+                      bordered
+                      placeholder="Description"
+                      onChangeText={handleChange('description')}
+                    />
+                    <View style={styles.imageRow}>
+                      <ImgPicker />
+                      <ImgPicker />
+                      <ImgPicker />
+                    </View>
+                    <View style={styles.imageRow}>
+                      <ImgPicker />
+                      <ImgPicker />
+                      <ImgPicker />
+                    </View>
+                    <Item regular>
+                      <Input
+                        onChangeText={handleChange('price')}
+                        onBlur={handleBlur('price')}
+                        value={values.price}
+                        placeholder="price"
+                      />
+                    </Item>
+                  </View>
+                  <View>
+                    <Button onPress={handleSubmit} style={styles.justifyCenter}>
+                      <Text>Submit</Text>
+                    </Button>
+                  </View>
+                </Form>
+              )}
+            </Formik>
+          </View>
+        </ImageBackground>
+      </Content>
+    </Container>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: { backgroundColor: '#4286f4', flex: 1 },
+  container: { flex: 1 },
   flex: { flex: 1 },
+  opacity: { opacity: 0.5 },
   grow: { flexGrow: 1 },
   imageRow: { flexDirection: 'row', justifyContent: 'space-evenly' },
   justifyCenter: { justifyContent: 'center' },
-  textCenter: { textAlign: 'center' },
+  formTitle: { textAlign: 'center', fontSize: 32, fontWeight: 'bold' },
 });
 
 export default PostNewScreen;
