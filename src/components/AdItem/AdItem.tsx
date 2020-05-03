6361fe61257657379cd1bebb68b2b823af1d447f
Justinas Reigis <justinas.reigis@gmail.com>
2020-05-03 16:44:38 +0300

    style: ad item in the list

--- src/components/AdItem/AdItem.tsx
+++ src/components/AdItem/AdItem.tsx
@@ -15,10 +15,10 @@ const AdItem: React.FC<any> = ({ item }) => {
   return (
     <Item onPress={() => console.log('should open detailed item view')}>
       <View style={styles.container}>
-        <Image source={{ uri: imgUrl }} style={styles.image} resizeMode="contain" />
+        <Image source={{ uri: imgUrl }} style={styles.image} />
         <View style={styles.content}>
           <Text style={styles.title}>{title}</Text>
-          <Text>{description}</Text>
+          <Text style={styles.description}>{description}</Text>
           <Text style={styles.price}>Price: {price}</Text>
         </View>
       </View>
@@ -27,16 +27,12 @@ const AdItem: React.FC<any> = ({ item }) => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    padding: 10,
-    backgroundColor: '#e2e8f0',
-  },
+  container: { flex: 1, flexDirection: 'row', padding: 10, backgroundColor: '#e2e8f0' },
   image: { flex: 1 },
   content: { flex: 2, paddingLeft: 20 },
-  title: { fontSize: 16, fontWeight: 'bold' },
-  price: { textAlign: 'center' },
+  title: { fontSize: 24, fontWeight: '500', textAlign: 'center', textTransform: 'capitalize' },
+  description: { fontSize: 14, paddingHorizontal: 20 },
+  price: { textAlign: 'center', fontSize: 12 },
 });
 
 export default AdItem;
